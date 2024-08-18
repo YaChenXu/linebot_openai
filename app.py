45d61bb7c59ce1cf7502bfd544a4141151089b64
YaChenXu <137896596+YaChenXu@users.noreply.github.com>
2024-08-18 20:03:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def handle_message(event):
     msg = event.message.text
     GPT_answer = GPT_response(msg)
     print(GPT_answer)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage('test2'))
 
 @handler.add(PostbackEvent)
 def handle_message(event):
